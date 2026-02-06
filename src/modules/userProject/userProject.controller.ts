b5633423105f773669510554b5015b67ad7d21bf
Filimon4 <f.rykov@bk.ru>
2026-02-06 10:18:18 +0300

    draw

---
+++ src/modules/userProject/userProject.controller.ts
@@ -0,0 +1,12 @@
+import { Controller } from "@nestjs/common";
+
+
+@Controller()
+export class UserProjectController {
+
+  // получить всех пользователей
+  // добавить пользователя
+  // заблокировать пользотеля
+  // получить данные подключённого пользователя
+
+}
